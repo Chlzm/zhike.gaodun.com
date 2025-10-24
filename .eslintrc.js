7157585688d19f51be2835bc5000aff767ca09b8
yourname <your@email.com>
2025-10-24 13:31:46 +0800

    save

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    // 可以在这里添加自定义规则
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
